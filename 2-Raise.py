3b8fe2b99c9a2bd9a2b39c266ebac59e30366b75
Savustyan <80714698+Savustyan@users.noreply.github.com>
2021-03-21 16:15:34 +0200

    Add files via upload

---
+++ 2-Raise.py
@@ -0,0 +1,14 @@
+# Не совсем понял 2 задание
+import random
+try:
+    raise random.choice([ZeroDivisionError, ImportError, KeyError, UnicodeError, StopIteration])
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ImportError:
+    print('ImportError')
+except KeyError:
+    print('KeyError')
+except UnicodeError:
+    print('UnicodeError')
+except StopIteration:
+    print('StopIteration')
